3d86b43ad2fbfc17db40f57f87acc51231a90091
RIKU <rank228130@gmail.com>
2021-07-20 23:29:29 +0900

    feature : dogList

---
+++ src/components/DogList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="dogList">
+    <div v-for="(image, imageIdx) in imagesInfo" :key="imageIdx">
+      <img class="image" :src="image" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DogList",
+  props: {
+    imagesInfo: {
+      type: Array,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style>
+.dogList {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+}
+
+.image {
+  max-width: 250px;
+  height: 250px;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
