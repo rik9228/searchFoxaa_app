86cbadd53bdf65a8a1efa72c04e197bb6bb7bc00
RIKU <rank228130@gmail.com>
2021-07-27 00:12:42 +0900

    update: compnent name to game-start by HelloWorld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="https://blogimg.goo.ne.jp/user_image/3f/13/fe1f78ded2409e795b25be9d48cc1e0a.png" />
-    <HelloWorld />
-  </div>
+  <game-start />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import GameStart from "../components/GameStart";
 
 export default {
   name: "Home",
+
   components: {
-    HelloWorld,
+    GameStart,
   },
 };
 </script>
