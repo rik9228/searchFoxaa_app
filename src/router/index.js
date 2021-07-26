e76ba7abe82e81aa5c6f6bf20c3e079db079bd43
RIKU <rank228130@gmail.com>
2021-07-27 00:10:27 +0900

    update: about to game by path

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,12 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about/game",
-    name: "About",
+    path: "/game",
+    name: "Game",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import(/* webpackChunkName: "" */ "../views/Game.vue"),
     props: true,
   },
 ];
