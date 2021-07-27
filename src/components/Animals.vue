45d7bd51aa0bc12cc1a478bd3b5962f216dad25e
RIKU <rank228130@gmail.com>
2021-07-27 21:46:00 +0900

    feature: Animals

---
+++ src/components/Animals.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="shuffleInfo">並べ替え</button>
+    <transition-group tag="div" class="dogList">
+      <div class="images" v-for="image in imageInfos" :key="image.id">
+        <animal :image="image"></animal>
+      </div>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import animal from "@/components/Animal.vue";
+export default {
+  name: "Animals",
+  components: {
+    animal,
+  },
+  props: {
+    imageInfos: {
+      type: Array,
+      require: true,
+    },
+  },
+  methods: {
+    shuffleInfo() {
+      this.$emit("shuffleInfo", this.imageInfos);
+    },
+  },
+};
+</script>
