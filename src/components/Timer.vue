1901c1f21d517b77c11790b3a84e60fd43b2b269
RIKU <rank228130@gmail.com>
2021-07-28 11:08:38 +0900

    feature: timer

---
+++ src/components/Timer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p class="text-h4 font-weight-medium"><font-awesome-icon icon="clock" size="xs" class="mb-1" />：{{ interval.toFixed(2) }}</p>
+    <!-- 小数2桁まで表示 -->
+    <!-- <button @click="startTimer()" v-show="!active">Start</button> -->
+    <!-- <button @click="stopTimer()" v-show="active">Stop</button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Timer",
+  data() {
+    return {
+      active: false, // 実行状態
+      start: 0, // startを押した時刻
+      timer: 0, // setInterval()の格納用
+      interval: 0, // 計測時間
+      accum: 0, // 累積時間(stopしたとき用)
+    };
+  },
+  created() {
+    this.active = true;
+    this.start = Date.now();
+    this.timer = setInterval(() => {
+      this.interval = this.accum + (Date.now() - this.start) * 0.001;
+    }, 10); // 10msごとに現在時刻とstartを押した時刻の差を足す
+  },
+  methods: {
+    stopTimer() {
+      this.active = false;
+      this.accum = this.interval;
+      clearInterval(this.timer);
+    },
+    stopTimerHandler() {
+      this.stopTimer();
+      this.$emit("stopTimerHandler");
+    },
+  },
+};
+</script>
+
+<style></style>
