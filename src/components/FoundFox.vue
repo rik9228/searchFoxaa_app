14b409309b8c6fb6038e0971863cbc79661e3f0d
RIKU <rank228130@gmail.com>
2021-07-28 11:14:04 +0900

    feature: foundFox component
    to use other occasion

---
+++ src/components/FoundFox.vue
@@ -0,0 +1,15 @@
+<template>
+  <animal />
+</template>
+
+<script>
+import animal from "/@comopnents/animal.vue";
+export default {
+  name: "FoundFox",
+  components: {
+    animal,
+  },
+};
+</script>
+
+<style></style>
