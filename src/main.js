a4cc7cc2db9eb189eca693caa628fff70ab271ed
RIKU <rank228130@gmail.com>
2021-07-28 11:10:51 +0900

    feature : FontAwesomeIcon

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import css from "./assets/css/style.css";
 import store from "@/store.js";
-import vuetify from './plugins/vuetify'
+import vuetify from "./plugins/vuetify";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faClock);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
@@ -12,5 +20,5 @@ new Vue({
   router,
   css,
   vuetify,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
