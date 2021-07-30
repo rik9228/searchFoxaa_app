76e7aff074d8a6875d489444f2a82a167bc38ed4
RIKU <rank228130@gmail.com>
2021-07-31 02:29:56 +0900

    feature: dialog of diffcaltymenu

--- src/components/GameStart.vue
+++ src/components/GameStart.vue
@@ -4,43 +4,51 @@
     <img class="mainVisual" alt="Vue logo" src="https://blogimg.goo.ne.jp/user_image/3f/13/fe1f78ded2409e795b25be9d48cc1e0a.png" />
     <h2 class="text-center">動物の中に紛れたキツネを探し当てて下さい。</h2>
     <p class="mt-5">🦊&emsp;難易度を選択する&emsp;🦊</p>
-    <v-container>
+    <p><b>※音声が流れます。</b></p>
+    <!-- <v-container> -->
+    <div class="col-6 mx-auto">
       <v-row>
         <!-- v-forを使うときにtemplateが有効 -->
         <template v-for="(difficalty, index) in difficalties">
           <v-col :key="index">
-            <v-btn :color="difficalty.color" dark @click="difficaltySelect(difficalty.key)">
+            <v-btn @click="difficaltySelect(difficalty.key)" :color="difficalty.color" :width="'100%'" dark>
               {{ difficalty.name }}
             </v-btn>
           </v-col>
         </template>
       </v-row>
 
-      <v-card class="mt-5" v-if="isDifficaltySelected">
-        <v-card-title class="text-h5 grey lighten-2">
-          難易度：
-          <v-rating
-            :value="selectedDifficalty.rating"
-            :readonly="true"
-            :dense="true"
-            :background-color="selectedDifficalty.color"
-            :color="selectedDifficalty.color"
-            large
-            class="ml-2"
-          ></v-rating>
-        </v-card-title>
+      <v-dialog v-model="dialog" width="600px">
+        <v-card v-if="isDifficaltySelected" class="pb-5">
+          <v-card-title class="text-h5 grey lighten-2 justify-center">
+            難易度：
+            <v-rating
+              :value="selectedDifficalty.rating"
+              :readonly="true"
+              :dense="true"
+              :background-color="selectedDifficalty.color"
+              :color="selectedDifficalty.color"
+              large
+              class="ml-2"
+            ></v-rating>
+          </v-card-title>
 
-        <v-card-text class="pt-5">犬の数：{{ selectedDifficalty.animals }}匹</v-card-text>
+          <v-card-text class="pt-5">犬の数：{{ selectedDifficalty.animals }}匹</v-card-text>
 
-        <v-card-text class="font-weight-bold">見つける狐の数：{{ selectedDifficalty.fox }}匹 </v-card-text>
-      </v-card>
-    </v-container>
+          <v-card-text class="font-weight-bold">見つける狐の数：{{ selectedDifficalty.fox }}匹 </v-card-text>
 
-    <router-link to="/game" event="" @click.native="Start">
-      <v-btn v-if="isDifficaltySelected" color="primary" class="mt-5">
-        狐を探しに行く
-      </v-btn>
-    </router-link>
+          <v-card-text v-if="selectedDifficaltyKey === 'hard'" class="font-weight-bold">※数秒ごとに画像がシャッフルされます</v-card-text>
+
+          <router-link to="/game" event="" @click.native="Start">
+            <v-btn v-if="isDifficaltySelected" color="primary" class="mt-5">
+              狐を探しに行く
+            </v-btn>
+          </router-link>
+        </v-card>
+      </v-dialog>
+    </div>
+
+    <!-- </v-container> -->
   </div>
 </template>
 
@@ -56,7 +64,7 @@ export default {
         {
           key: "easy",
           name: "草むら",
-          animals: 5,
+          animals: 10,
           fox: 2,
           rating: 1,
           color: "success",
@@ -78,10 +86,11 @@ export default {
           color: "purple",
         },
       ],
-      selectedDifficaltyKey: "",
+      selectedDifficaltyKey: "easy",
       dialog: false,
     };
   },
+
   computed: {
     selectedDifficalty() {
       return this.difficalties.find((difficalty) => difficalty.key === this.selectedDifficaltyKey);
@@ -99,13 +108,13 @@ export default {
       Router.push({
         name: "Game",
         params: {
-          difficalties: this.difficalty,
-          facts: this.facts,
+          selectedDifficalty: this.selectedDifficalty,
         },
       });
     },
     difficaltySelect(difficaltyKey) {
       this.selectedDifficaltyKey = difficaltyKey;
+      this.dialog = !this.dialog;
     },
   },
 };
