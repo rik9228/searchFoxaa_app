eb251b05c1601772eb50ee0ba34d7cddf2f43091
RIKU <rank228130@gmail.com>
2021-08-04 16:22:04 +0900

    delete: showResult, showResultHandler
    to props accTime

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,42 +1,48 @@
 <template>
   <div class="text-center">
-    <v-dialog v-model="dialog">
-      <v-card>
-        <v-card-title class="text-h5 grey lighten-2 justify-center">
-          狐を全て探し出しました！
-        </v-card-title>
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2 justify-center">
+        狐を全て探し出しました！
+      </v-card-title>
 
-        <v-card-text class="pt-5 pb-2">見つけた狐</v-card-text>
+      <v-card-text class="pt-5 pb-2 text-h6">見つけた狐</v-card-text>
 
-        <div class="result-dialog__wrapper pt-4 pb-8">
-          <figure v-for="foundFox in foundFoxes" :key="foundFox.id">
-            <img :src="foundFox" class="result-dialog__image" />
-          </figure>
-        </div>
+      <div class="result-dialog__wrapper pt-4 pb-8">
+        <figure v-for="foundFox in foundFoxes" :key="foundFox.id">
+          <img :src="foundFox" class="result-dialog__image" />
+        </figure>
+      </div>
 
-        <v-card-text class="pt-5 pb-2">間違えた数：{{ wrongCount }}</v-card-text>
+      <div class="pa-5">
+        <v-card-text class="pa-0 text-h6">間違えた数：{{ wrongCount }}</v-card-text>
+        <v-card-text class="pa-0 text-h6">かかった時間：{{ accTime.toFixed(2) }}秒</v-card-text>
+      </div>
 
-        <v-divider></v-divider>
+      <v-divider></v-divider>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="dialog = false">
-            <router-link to="/">
-              もう一度トライする
-            </router-link>
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text @click="dialog = false">
+          <router-link to="/">
+            もう一度トライする
+          </router-link>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script>
+// import { mapGetters } from "vuex";
 export default {
   name: "result",
   props: {
     foundFoxes: {
       type: Array,
+      require: true,
+    },
+    accTime: {
+      type: Number,
       require: true,
     },
     wrongCount: {
@@ -49,20 +55,14 @@ export default {
       dialog: false,
       resultData: {
         かかった時間: 0,
-        wrongCount: 0,
       },
     };
   },
-  methods: {
-    showResult() {
-      this.dialog = true;
-      this.resultData.wrongCount = this.wrongCount;
-      // localStorage.setItem("結果", JSON.stringify(this.resultData));
-    },
-    showResultHandler() {
-      this.showResult();
-      this.$emit("showResultHandler");
-    },
+  computed: {
+    // ...mapGetters({
+    //   wrongCount: "game/wrongCount",
+    //   accumTime: "game/accumTime",
+    // }),
   },
 };
 </script>
