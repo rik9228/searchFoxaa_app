08b4a7dd10f4e8dcd10e9c2dcc8194c56f56990d
RIKU <rank228130@gmail.com>
2021-08-13 02:23:04 +0900

    fix: to get localStorage method
    to handling localStorage Item in store

--- src/components/GameStart.vue
+++ src/components/GameStart.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="hello">
-    <h1>キツネ探し当てゲーム</h1>
-    <img class="mainVisual" alt="Vue logo" src="https://blogimg.goo.ne.jp/user_image/3f/13/fe1f78ded2409e795b25be9d48cc1e0a.png" />
-    <h2 class="text-center">動物の中に紛れたキツネを探し当てて下さい。</h2>
+    <img
+      class="mainVisual"
+      alt="Vue logo"
+      src="https://blogimg.goo.ne.jp/user_image/3f/13/fe1f78ded2409e795b25be9d48cc1e0a.png"
+    />
+    <h2 class="text-center">ワンちゃんの中に紛れたキツネを探し当てて下さい。</h2>
     <p class="mt-5">🦊&emsp;難易度を選択する&emsp;🦊</p>
     <div class="col-6 mx-auto">
       <v-row>
         <template v-for="(difficalty, index) in difficalties">
           <v-col :key="index">
-            <v-btn @click="difficaltySelect(difficalty.key)" :color="difficalty.color" :width="'100%'" dark>
+            <v-btn
+              @click="difficaltySelect(difficalty.key)"
+              :color="difficalty.color"
+              :width="'100%'"
+              dark
+            >
               {{ difficalty.name }}
             </v-btn>
           </v-col>
         </template>
       </v-row>
-      <v-card class="mt-5 mx-auto pa-3" max-width="600">
+      <v-card class="mt-15 mx-auto pa-3" max-width="600">
         <v-card-title class="justify-center"><b>これまでの結果</b></v-card-title>
         <template v-for="(resultHistory, index) in resultHistories">
           <div class="d-flex" :key="index">
@@ -40,13 +48,23 @@
             ></v-rating>
           </v-card-title>
 
-          <v-card-text class="pt-5">犬の数：{{ selectedDifficalty.animals }}匹</v-card-text>
+          <v-card-text class="pt-5"
+            >犬の数：{{ selectedDifficalty.animals }}匹</v-card-text
+          >
 
-          <v-card-text class="font-weight-bold">見つける狐の数：{{ selectedDifficalty.fox }}匹 </v-card-text>
+          <v-card-text class="font-weight-bold"
+            >見つける狐の数：{{ selectedDifficalty.fox }}匹
+          </v-card-text>
 
-          <v-card-text class="font-weight-bold text--primary pb-1"><b>※音声が流れます。</b></v-card-text>
+          <v-card-text class="font-weight-bold text--primary pb-1"
+            ><b>※音声が流れます。</b></v-card-text
+          >
 
-          <v-card-text v-if="selectedDifficaltyKey === 'hard'" class="font-weight-bold pb-1 pt-2">※数秒ごとに画像がシャッフルされます</v-card-text>
+          <v-card-text
+            v-if="selectedDifficaltyKey === 'hard'"
+            class="font-weight-bold pb-1 pt-2"
+            >※数秒ごとに画像がシャッフルされます</v-card-text
+          >
 
           <router-link to="/game" event="" @click.native="Start">
             <v-btn v-if="isDifficaltySelected" color="primary" class="mt-5">
@@ -61,6 +79,7 @@
 
 <script>
 import Router from "@/router/index.js";
+import { mapGetters } from "vuex";
 export default {
   name: "GameStart",
   data() {
@@ -97,15 +116,20 @@ export default {
     };
   },
   created() {
-    this.resultHistories = JSON.parse(localStorage.getItem("result"));
+    this.resultHistories = this.lastFiveResults;
   },
   computed: {
     selectedDifficalty() {
-      return this.difficalties.find((difficalty) => difficalty.key === this.selectedDifficaltyKey);
+      return this.difficalties.find(
+        (difficalty) => difficalty.key === this.selectedDifficaltyKey
+      );
     },
     isDifficaltySelected() {
       return this.selectedDifficaltyKey !== "";
     },
+    ...mapGetters({
+      lastFiveResults: "game/lastFiveResults",
+    }),
   },
   methods: {
     // v-btnのtoへのバインドも考える
