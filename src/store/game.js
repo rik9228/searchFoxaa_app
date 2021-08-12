0945ed5e194afb96602cf530d202a6ac5f852f26
RIKU <rank228130@gmail.com>
2021-08-13 02:26:41 +0900

    fix: updating mutations
    to prevent from doing anything other than updating their state.

--- src/store/game.js
+++ src/store/game.js
@@ -2,20 +2,45 @@ const state = {
   resultHistories: [],
 };
 const getters = {
-  resultHistories(state) {
-    return state.resultHistories;
-  },
+  resultHistories: (state) => state.resultHistories,
+  lastFiveResults: (state) => {
+    function reverse(arr) {
+      if (toString.call(arr) !== '[object Array]') return null;
+      if (arr.length === 0) return arr;
+      const copy = arr.slice();
+      return copy.reverse();
+    }
+    return reverse(state.resultHistories).slice(0, 5);
+  }
 };
 const mutations = {
-  setResultHistory(state, payload) {
-    if (state.resultHistories.length === 5) {
-      state.resultHistories.shift();
-    }
-    state.resultHistories.push(payload);
-    localStorage.setItem("result", JSON.stringify(state.resultHistories));
+  initialize(state) {
+    state.resultHistories = [];
+  },
+  resultHistories(state, result) {
+    state.resultHistories = result;
+  },
+  addResultHistory(state, result) {
+    state.resultHistories.push(result);
   },
 };
-const actions = {};
+const actions = {
+  initialize(context) {
+    context.commit('initialize');
+  },
+  loadResultHistory(context) {
+    const result = JSON.parse(localStorage.getItem("result"));
+    if (result === null) {
+      context.commit('resultHistories', []);
+      return;
+    }
+    context.commit('resultHistories', result);
+  },
+  saveResultHistory(context, result) {
+    context.commit('addResultHistory', result);
+    localStorage.setItem("result", JSON.stringify(context.state.resultHistories));
+  }
+};
 
 export default {
   namespaced: true,
